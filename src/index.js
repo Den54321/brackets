69480376e9946d3255dca2ed1e933449b540e966
Den54321 <dennbuika@gmail.com>
2023-01-29 14:35:34 +0300

    docs: add salution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  newStr = str;
+  for(let i = 0; i < bracketsConfig.length; i++)
+  {
+    for(let j = 0; j < str.length; j++)
+    {
+      if(bracketsConfig[i][0] == str[j] && bracketsConfig[i][1] == str[j + 1])
+      {
+        str = str.replace(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`,'');
+        i = -1;
+        break;
+      }  
+    }                 
+  }
+  return str === "" ? true : false;
 }
